c4319bd827b48c411f6622e721310fb60b89e587
Bamburas <scierwobox@gmail.com>
2012-12-27 16:27:39 +0100

    xxxx

--- BlackLagoonProject/test.php
+++ BlackLagoonProject/test.php
@@ -1,73 +1,11 @@
-<?php
 
-class CodeCallGtk extends GtkWindow {
+<html>
+<head>
+<title> Ale bigiel prosi ;p</title>
+</head>
+<body>
+<audio src="http://localhost/rockandroll.ogg" controls preload> </audio>
+</body>
 
+</html>
 
-
-	/**
-
-	* FirstGTK constructor. Actually creates the
-
-	* window
-
-	*
-
-	* @return FirstGtk
-
-	*/
-
-	public function __construct() {
-
-		/*
-
-		* Call the parent constructor
-
-		*/
-
-		parent::__construct();
-
-
-
-		/*
-
-		* Set the size of the new GTK Window
-
-		* Set the Title
-
-		* Connect the destroy/exit event to a function
-
-		*/
-
-		$this->set_default_size(400,100);
-
-		$this->set_title('CodeCall.net PHP-GTK GUI Tutorial');
-
-		$this->connect_simple('destroy', array('gtk', 'main_quit'));
-
-
-		/*
-
-		* This will realize and show
-
-		* all windows from child to parent.
-
-		*/
-
-		$this->show_all();
-
-	}
-
-}
-
-
-
-new CodeCallGtk();
-
-Gtk::main();
-
-
-
-
-
-
-?>
